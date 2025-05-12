a96b4c2f8ec3eeaf15dcf3242e8884f9cb55f871
sohel730 <momin8805887375@gmail.com>
2025-05-12 22:59:46 +0930

    Add files via upload
    project added

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+// import './index.css'
+import NavBar from './Components/NavBar/NavBar'
+import {Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Video from './Pages/Video/Video'
+
+const App = () => {
+
+const [sidebar,setSidebar] = useState(true);
+
+  return (
+    <div>
+      <NavBar setSidebar={setSidebar}/>
+  <Routes>
+    <Route path='/' element={<Home sidebar={sidebar}/>}/>
+    <Route path='/video/:categoryId/:videoId' element={<Video/>}/>
+  </Routes>
+    </div>
+  )
+}
+
+export default App
